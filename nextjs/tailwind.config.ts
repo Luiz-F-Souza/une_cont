03e91b03c4e22ab05b4eff05129ed0321b270d1d
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-12-11 08:35:10 -0300

    first commit

---
+++ nextjs/tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors:{
+        brand:{
+          orange: {
+            200: "#FF9804",
+            500: "#ff8000"
+          }
+        }
+      },
+      borderWidth:{
+        1: "1px"
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
