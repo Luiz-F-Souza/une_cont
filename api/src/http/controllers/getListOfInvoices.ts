ebf157bd5d060b559b6c9488d9cee13e7a434c3c
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-12-11 19:55:31 -0300

    chore: clears unnecessary imports and white spaces

--- api/src/http/controllers/getListOfInvoices.ts
+++ api/src/http/controllers/getListOfInvoices.ts
@@ -3,23 +3,17 @@ import { z } from "zod"
 import { PrismaInvocesRepository } from "../../repositories/prisma/invoices-repository"
 import { getAllInvoicesUseCase } from "../../use-cases/getAllInvoices"
 
-export async function getListOfInvoices(request: FastifyRequest, reply: FastifyReply) {
+export async function getListOfInvoices(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
 
-    // const checkinHistoryQuerySchema = z.object({
-    //   page: z.coerce.number().min(1).default(1)
-    // })
-  
-    // const { page } = checkinHistoryQuerySchema.parse(request.query)
+  const invoicesRepository = new PrismaInvocesRepository()
+  const useCase = new getAllInvoicesUseCase(invoicesRepository)
 
-    const invoicesRepository = new PrismaInvocesRepository()
-    const useCase = new getAllInvoicesUseCase(invoicesRepository)
-  
-    const { invoices } = await useCase.execute()
-  
-  
-    return reply.status(200).send({
-      data: {
-        invoices
-      }
-    })
-  }
+  const { invoices } = await useCase.execute()
+
+  return reply.status(200).send({
+    data: invoices,
+  })
+}
--- api/src/use-cases/getAllInvoices.ts
+++ api/src/use-cases/getAllInvoices.ts
@@ -1,32 +1,51 @@
-
-
-import { invoices } from "@prisma/client";
-import { InvoicesInterface } from "../repositories/@interface/invoices-interface";
-import {Dictionary, groupBy} from 'lodash'
-import { format } from "date-fns";
-import { ptBR } from "date-fns/locale";
-
-
+import { invoices } from "@prisma/client"
+import { InvoicesInterface } from "../repositories/@interface/invoices-interface"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 interface ReplyType {
-  invoices:  Dictionary<invoices[] | null>,
+  invoices: GroupedInvoices[] | null
 }
 
+type GroupedInvoices = {
+  month: string
+  totalInMonth: number
+  invoices: invoices[]
+}
 export class getAllInvoicesUseCase {
-
-  constructor(private invoicesRepository: InvoicesInterface) { }
+  constructor(private invoicesRepository: InvoicesInterface) {}
 
   async execute(): Promise<ReplyType> {
-
     const invoices = await this.invoicesRepository.getAllInvoices()
 
-  
-    const invoicesPerMonth = groupBy(invoices, (invoice) => {
-        return format(invoice.createdAt, 'MMMM', {locale: ptBR})
-    })
+    const invoicesGroupedByMonth: GroupedInvoices[] | undefined =
+      invoices?.reduce((groupedArr: GroupedInvoices[], invoice) => {
+        const monthName = format(invoice.createdAt, "MMMM", { locale: ptBR })
+        const registeredMonth = groupedArr.find(
+          (registeredMonth) => registeredMonth.month === monthName
+        )
+
+        const isInvoicePaid = invoice.status === 4
+
+        const currentPaidValue = isInvoicePaid ? invoice.value : 0
+
+        if (registeredMonth) {
+          registeredMonth.totalInMonth += currentPaidValue
+
+          registeredMonth.invoices.push(invoice)
+        } else {
+          groupedArr.push({
+            month: monthName,
+            totalInMonth: currentPaidValue,
+            invoices: [invoice],
+          })
+        }
+
+        return groupedArr
+      }, [])
+
     return {
-        invoices: invoicesPerMonth,
+      invoices: invoicesGroupedByMonth ?? null,
     }
   }
-
 }
