5659980264a8be8c2d3d275d52aa1a3f3872b909
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-12-12 17:29:24 -0300

    feat: substitui chamada localhost:8888 pelo ip do servidor

--- nextjs/src/apiCalls/invoices.ts
+++ nextjs/src/apiCalls/invoices.ts
@@ -1,14 +1,14 @@
 import { ListOfInvoices, YearSummary } from "@/@types/dto"
 
 export const getListOfInvoices = async () => {
-  const response = await fetch("http://localhost:8888/all-invoices")
+  const response = await fetch("http://146.190.40.76:8888/all-invoices")
   const { data: listOfInvoices } = (await response.json()) as ListOfInvoices
 
   return { listOfInvoices }
 }
 
 export const getYearSummary = async () => {
-  const response = await fetch("http://localhost:8888/year-summary")
+  const response = await fetch("http://146.190.40.76:8888/year-summary")
   const { data: yearSummary } = (await response.json()) as YearSummary
 
   return { yearSummary }
