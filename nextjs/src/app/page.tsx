86f95ac1b4068a9a78e678b9b4d3a0c5230c96fe
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-12-12 17:30:58 -0300

    fix: corrige redirecionamento na home

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client"
 
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function Home() {
 
-  useRouter().replace('/dashboard')
+  const router = useRouter()
+  useEffect(() => {
+    router.push('/dashboard')
+  },[router])
 
   return null
 
