2f646c098842f64590eeabfe91771690e1bccda3
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-12-12 04:38:55 -0300

    fix: altera container de div para button para mais semântica e acessibilidade

--- nextjs/src/components/Header/ToggleMenuButton.tsx
+++ nextjs/src/components/Header/ToggleMenuButton.tsx
@@ -6,13 +6,13 @@ import { menuStore } from "../store/menuStore"
 export const ToggleMenuButton = () => {
   const { isMobileMenuOpen, setIsMobileMenuOpen } = menuStore()
   return (
-    <div className="z-50" onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
+    <button className="z-50" onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
       {!isMobileMenuOpen && (
         <IoIosMenu className="w-8 h-8 hover:cursor-pointer lg:hidden" />
       )}
       {isMobileMenuOpen && (
         <IoIosCloseCircleOutline className="w-8 h-8 hover:cursor-pointer lg:hidden" />
       )}
-    </div>
+    </button>
   )
 }
