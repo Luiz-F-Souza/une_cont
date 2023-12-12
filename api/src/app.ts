7d85f9e587b10293e996d4701fc7b0bb884db1ce
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-12-12 04:29:43 -0300

    feat: cors adicionado para ip do liveServer

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,9 +2,15 @@ import fastify from 'fastify'
 import { appRoutes } from './http/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
+import cors from '@fastify/cors'
 
 
 export const app = fastify()
+
+app.register(cors, {
+  origin: 'http://127.0.0.1:5500',
+  methods:  ['GET', 'PUT', 'POST', 'DELETE']
+})
 
 app.setErrorHandler((err, _request, reply) => {
 
